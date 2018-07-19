58add62aae8d1b90a84a51e64f5a57bf7d1d45ec
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-18 18:17:32 -0700

    Unit test: Validates a good model

--- test/unit/tour.test.js
+++ test/unit/tour.test.js
@@ -1,2 +1,33 @@
 const { assert } = require('chai');
 const Tour = require('../../lib/models/tour');
+const { getErrors } = require('./helpers');
+
+describe('Tour model', () => {
+
+    it('Validates a good model', () => {
+        const data = {
+            title: 'Circus Clowns',
+            activities: ['It scares you', 'Bobo the clown juggles'],
+            launchDate: new Date(),
+            stops: {
+                location: {
+                    city: 'Tualatin',
+                    state: 'Oregon',
+                    zip: 97062
+                },
+                weather: {
+                    temperature: 85,
+                    sunset: '8:14pm'
+                },
+                attendance: 52
+            }
+        };
+
+        const tour = new Tour(data);
+        const json = tour.toJSON();
+        delete json._id;
+        assert.deepEqual(json, data);
+
+    });
+
+});
